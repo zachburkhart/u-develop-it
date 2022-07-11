69218cce6eb390a23be49f3881c237d3ad2fea6f
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-11 08:08:01 -0400

    query the database to test the connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +18,23 @@ app.use((req, res) => {
     res.status(404).end();
   });
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'B00165036',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+    
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
